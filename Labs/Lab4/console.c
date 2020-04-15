34e6ee1ad15e6d78ddefd17bb682b6322bf422ed
magos3 <taba>
2020-04-15 06:55:10 +0100

    p

--- Labs/Lab4/console.c
+++ Labs/Lab4/console.c
@@ -2,11 +2,13 @@
 #include <io.h>
 #include <libc.h>
 
-#define COLOR_BASE   0xB8000   /* base of color video memory. 32KB*/
-#define COLOR_SIZE    0x4000   /* 16K color video memory */
+#define VIDEO_BASE   0xB8000   /* base of color video memory. 32KB*/
+#define VIDEO_SIZE    0x4000   /* 16K color video memory */
 
 /* VGA CRT controller chips. */
-#define C_GD5446_INDEX         0x3D4   /* port for 6845 color */
+#define GD5446_INDEX         0x3D4   /* GD5446 index register */
+#define GD5446_DATA          0x3D5     /* GD5446 data register  */
+
 #define INDEX              0   /* 6845's index register */
 #define DATA               1   /* 6845's data register */
 
@@ -21,8 +23,6 @@
 #define ERASE_CHAR             (DEFAULT_ATTR | 0x00)
 
 
-int video_port;		/* I/O port for accessing 6845 */
-
 typedef struct console {
   int row;               /*current line*/
   int col;               /*current column*/
@@ -34,50 +34,49 @@ typedef struct console {
   int  size;             /* size of video memory */
   long limit;            /* limit of this console's video memory */
 } console_t;
-
+ 
 console_t consoles[NR_CONS];
 console_t *curcons; /*currently visible*/
 
-#define pos(cons,row,col) (unsigned short *)(cons->start + cons->origin*2 + (row)*SCR_WIDTH*2 + (col)*2)
-#define cursor(cons) (cons->origin + cons->col + cons->row*SCR_WIDTH)
+#define mem_pos(cons,row,col) (unsigned short *)(cons->start + cons->origin*2 + (row)*SCR_WIDTH*2 + (col)*2)
+#define cursor(cons) (cons->origin + cons->row*SCR_WIDTH  + cons->col )
 
-void set_6845(unsigned short value, int reg);
+void set_gd5446(unsigned short value, int reg);
 
 void cons_clear(int tty)
 {
   console_t *cons = &consoles[tty];
-  memsetw ((short*)pos(cons,0,0), cons->blank, COLOR_SIZE);
+  memsetw ((short*)mem_pos(cons,0,0), cons->blank, VIDEO_SIZE);
   cons->col = cons->row = 0;
 }
 
 void cons_init()
 {
   curcons = &consoles[0];
-  curcons->start = COLOR_BASE; 
+  curcons->start = VIDEO_BASE; 
   curcons->origin = 0;
-  curcons->cursor = 0; 
+  curcons->cursor = 0
   curcons->col = curcons->row = 0;
   curcons->attr = DEFAULT_ATTR;
   curcons->blank = ERASE_CHAR;
   curcons->size = SCR_SIZE;
-  video_port = C_6845;
   cons_clear (0);
 }
 
 
 /* in:
- *   reg: which register pair to set
+ *   index: set index register register
  *   value : 16-bit value to set it to
- * Set a register pair inside the 6845.
- * Registers 12-13 tell the 6845 where in video ram to start(origin)
- * Registers 14-15 tell the 6845 where to put the cursor
+ * Set a register pair inside the gd5446.
+ * indexes 0Ch-0Dh tell the 5446 where in video ram to start(origin)
+ * indexes 0Eh-0Fh tell the 5446 where to put the cursor
  */
-void set_6845(unsigned short value, int reg)
+void set_gd5446(unsigned short value, int index)
 {
-  outb(reg, video_port + INDEX); /*set the index register*/
-  outb((value >> 8) & 0xff, video_port + DATA);  /*output high byte*/
-  outb(reg + 1, video_port + INDEX);
-  outb(value & 0xff, video_port + DATA);  /*output low byte*/
+  outb(index,  GD5446_INDEX ); /*set the index register*/
+  outb((value >> 8) & 0xff,   GD5446_DATA);  /*output high byte*/
+  outb(index + 1,   GD5446_INDEX);
+  outb(value & 0xff,   GD5446_DATA);  /*output low byte*/
 }
 
 
