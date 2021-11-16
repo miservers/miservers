a868ba7557b8fc239ac27bdc4b5b8f76cfa28825
magos3 <60229491+magos3@users.noreply.github.com>
2021-11-16 01:07:08 +0100

    commit

--- workspace/programing/useState-from-scratch.js
+++ workspace/programing/useState-from-scratch.js
@@ -1,16 +1,18 @@
 
-const useState = (initVal) => {
-    let _state = initVal
-    let state = () => _state
-    let setState = (newState) => {
-        _state = newState
-    }
-    return [state, setState]
+const React = () => {
+    let state =0
 }
+React.useState = (initVal) => {
+        React.state = initVal
+        let setState = (newState) => {
+            React.state = newState
+        }
+        return [React.state, setState]
+    }
 
-let [count, setCount] = useState(1)
+let [count, setCount] = React.useState(1)
 
-console.log(count())
-setCount(2);
-console.log(count())
 console.log(count)
+setCount(2);
+console.log(count)
+
