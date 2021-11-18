632b90a8812589be95afb823806acbd4e3e2d812
magos3 <60229491+magos3@users.noreply.github.com>
2021-11-19 00:36:27 +0100

    commit

--- workspace/programing/useState-from-scratch.js
+++ workspace/programing/useState-from-scratch.js
@@ -1,18 +1,42 @@
+// Delaring React using MODULE  Pattern 
 
-const React = () => {
-    let state =0
-}
-React.useState = (initVal) => {
-        React.state = initVal
-        let setState = (newState) => {
-            React.state = newState
+const React = (function(){
+  let self = {} // reference to thismodule
+  let state
+
+  // public functions
+  self.render  = function (Component)  {
+        const comp = Component()
+        comp.render() 
+        console.log('state: ', state)
+        return comp
+  }
+
+  self.useState = function (initValue) {
+        state = state || initValue
+        function setState(newState) {
+          state = newState;
         }
-        return [React.state, setState]
-    }
+        return [state, setState]
+  }
+ 
+  return self   
+})()
 
-let [count, setCount] = React.useState(1)
 
-console.log(count)
-setCount(2);
-console.log(count)
+const Counter = ()=>{
+  let [count, setCount] = React.useState(1)
+  
+  return {
+    click: () => {setCount(count+1)},
+    render: () => console.log('render: ', {count})
+  }
+}
 
+let app = React.render(Counter)
+app.click()
+app = React.render(Counter)
+app.click()
+app = React.render(Counter)
+
+//React.render(C)
