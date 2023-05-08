d8b9b939c5c2673fe6f1e3e6eff1fc4e2c835232
magos3 <60229491+magos3@users.noreply.github.com>
2023-05-08 16:47:17 +0100

    commit

---
+++ workspace/springboot/springboot-rest/src/test/java/dev/springboot/rest/SpringbootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.springboot.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
