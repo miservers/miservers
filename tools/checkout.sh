30b848844b05e51d64635d3f6b6b527dbb3e6206
magos3 <60229491+magos3@users.noreply.github.com>
2023-09-08 21:04:36 +0100

    commit

--- tools/checkout.sh
+++ tools/checkout.sh
@@ -1,2 +1,9 @@
 #ifup eth0
+
+dir=`dirname $0`
+
+cd $dir
+
 git pull
+
+cd -
--- tools/commit.sh
+++ tools/commit.sh
@@ -5,10 +5,14 @@
 #   ifup eth0
 #fi
 
+dir=`dirname $0`
+
+cd $dir
 
 git add -A
 git commit -a -m "commit"
 git push origin master
 
+cd -
 
 
