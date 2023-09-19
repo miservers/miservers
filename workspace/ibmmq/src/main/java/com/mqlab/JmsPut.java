9c972f30ab60b7fbb7dc8f1eca7486dcce886df1
magos3 <60229491+magos3@users.noreply.github.com>
2023-09-19 15:05:01 +0100

    commit

--- workspace/ibmmq/src/main/java/com/mqlab/JmsPut.java
+++ workspace/ibmmq/src/main/java/com/mqlab/JmsPut.java
@@ -58,7 +58,7 @@ public class JmsPut {
 	private static final int PORT = 1415; // Listener port for your queue manager
 	private static final String CHANNEL = "DEV.APP.SVRCONN"; // Channel name
 	private static final String QMGR = "DEV.QM1"; // Queue manager name
-	private static final String APP_USER = "app"; // User name that application uses to connect to MQ
+	private static final String APP_USER = "ali"; // User name that application uses to connect to MQ
 	private static final String APP_PASSWORD = "changeit"; // Password that the application uses to connect to MQ
 	private static final String QUEUE_NAME = "DEV.QUEUE.1"; // Queue that the application uses to put and get messages to and from
 
